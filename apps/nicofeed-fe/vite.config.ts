a8c4e608144fa5d5ee680b6a06173e45a9cf936a
WindSekirun <windsekirun@gmail.com>
2024-10-30 20:30:58 +0900

    Connect with vue, vuetify

---
+++ apps/nicofeed-fe/vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import VueRouter from 'unplugin-vue-router/vite';
+import Components from 'unplugin-vue-components/vite';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/nicofeed-fe',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [
+    Components({
+      /* options */
+    }),
+    VueRouter({
+      /* options */
+    }),
+    vue(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+  ],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  build: {
+    outDir: '../../dist/apps/nicofeed-fe',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
