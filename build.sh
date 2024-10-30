d13c20f322beda55e0a706844b8b427bd8cbae0b
WindSekirun <windsekirun@gmail.com>
2024-10-30 23:54:24 +0900

    remove unused file

--- build.sh
+++
@@ -1,3 +0,0 @@
-nx run-many --target=build
-docker buildx build --file apps/nicofeed/Dockerfile -t windsekirun/nicofeed .
-docker buildx build --file apps/nicofeed-fe/Dockerfile -t windsekirun/nicofeed-fe .
